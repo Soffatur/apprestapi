73424c01ecf4d3872875adb52c9dd40a2e9a1c2b
Soffatur <soffatur26@gmail.com>
2021-07-02 00:16:30 +0700

    show data mahasiswa (get)

--- controller.js
+++ controller.js
@@ -4,5 +4,15 @@ var response = require('./res');
 var connection = require('./koneksi');
 
 exports.index = function(req, res){
-    response.ok('Applisi REST API berjalan!');
+    response.ok('Applisi REST API berjalan!', res);
+}
+
+exports.getMahasiswa = function(req, res){
+    connection.query("SELECT * FROM mahasiswa", function(error, rows, fields){
+        if(error){
+            connection.log(error);
+        }else{
+            response.ok(rows, res);
+        }
+    });
 }
