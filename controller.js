0aa5d8e5da64a73c1a472822f97e166855ead64f
Soffatur <soffatur26@gmail.com>
2021-07-03 14:11:09 +0700

    put mahasiswa

--- controller.js
+++ controller.js
@@ -3,41 +3,62 @@
 var response = require('./res');
 var connection = require('./koneksi');
 
-exports.index = function(req, res){
+exports.index = function (req, res) {
     response.ok('Applisi REST API berjalan!', res);
 }
 
-exports.getMahasiswa = function(req, res){
-    connection.query("SELECT * FROM mahasiswa", function(error, rows, fields){
-        if(error){
+// get mahasiswa
+exports.getMahasiswa = function (req, res) {
+    connection.query("SELECT * FROM mahasiswa", function (error, rows, fields) {
+        if (error) {
             console.log(error);
-        }else{
+        } else {
             response.ok(rows, res);
         }
     });
 }
 
-exports.getMahasiswaID = function(req, res){
+// get mahasiswa where id
+exports.getMahasiswaID = function (req, res) {
     let id = req.params.id
-    connection.query(`SELECT * FROM mahasiswa WHERE id = ?`,[id], function(error, rows, fields){
-        if(error){
+    connection.query(`SELECT * FROM mahasiswa WHERE id = ?`, [id], function (error, rows, fields) {
+        if (error) {
             console.log(error)
-        }else{
+        } else {
             response.ok(rows, res)
         }
     })
 }
 
-exports.postMahasiswa = function(req, res){
+// post mahasiswa
+exports.postMahasiswa = function (req, res) {
     let nim = req.body.nim
     let nama = req.body.nama
     let jurusan = req.body.jurusan
 
-    connection.query('INSERT INTO mahasiswa (nim, nama, jurusan) VALUES (?,?,?)',[nim, nama, jurusan], function(error, rows, fields){
+    connection.query('INSERT INTO mahasiswa (nim, nama, jurusan) VALUES (?,?,?)', [nim, nama, jurusan], function (error, rows, fields) {
+        if (error) {
+            console.log(error)
+        } else {
+            response.ok('Data send Successfull!', res)
+        }
+    })
+}
+
+// put mahasiswa
+exports.putMahasiswa = function (req, res) {
+    let id = req.params.id
+    let nim = req.body.nim
+    let nama = req.body.nama
+    let jurusan = req.body.nama
+
+    connection.query("UPDATE mahasiswa SET nim = ?, nama = ?, jurusan = ? WHERE id = ?", [
+        nim, nama, jurusan, id
+    ], function (error, rows, fields){
         if(error){
             console.log(error)
         }else{
-            response.ok('Data send Successfull!', res)
+            response.ok(`Update data where id ${id} successfull!`, res)
         }
     })
 }
--- routes.js
+++ routes.js
@@ -4,7 +4,8 @@ module.exports = function(app){
     var jsonku = require('./controller');
 
     app.route('/').get(jsonku.index);
-    app.route('/mahasiswa').get(jsonku.getMahasiswa);
-    app.route('/mahasiswa/:id').get(jsonku.getMahasiswaID);
-    app.route('/mahasiswa').post(jsonku.postMahasiswa);
+    app.route('/mahasiswa').get(jsonku.getMahasiswa); //get mahasiswa
+    app.route('/mahasiswa/:id').get(jsonku.getMahasiswaID); //get mahasiswa where id
+    app.route('/mahasiswa').post(jsonku.postMahasiswa); //post mahasiswa
+    app.route('/mahasiswa/:id').put(jsonku.putMahasiswa); //put
 }
